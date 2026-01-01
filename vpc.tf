c162097dd67e7925b29dd91804943fbddc53e3b7
gguip1 <gguip7554@naver.com>
2026-01-01 16:11:22 +0900

    feat: add VPC endpoint route table association for S3

--- vpc.tf
+++ vpc.tf
@@ -107,3 +107,8 @@ resource "aws_vpc_endpoint" "s3" {
     Environment = "aws_vpc_endpoint"
   }
 }
+
+resource "aws_vpc_endpoint_route_table_association" "route_table_association_vpc_endpoint_s3" {
+  route_table_id  = aws_route_table.private.id
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+}
